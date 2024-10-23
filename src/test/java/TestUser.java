554962da70be97d35f5028da49d3462ae20e3b9d
juyamada <jysyamada@gmail.com>
2024-10-22 22:36:06 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,39 @@
+// 0 - nome do pacote
+
+// 1 - bibliotecas
+import org.junit.jupiter.api.Test;
+
+// 2 - classe
+
+
+
+public class TestUser {
+    // 2.1 - atributos
+    static String ct = "application/json"; // content-type
+    static String uriUser = "https://petstore.swagger.io/v2/user";
+
+    // 2.2 - funções e métodos
+    // 2.2.1 - funções e métodos comuns / uteis
+    
+    // Função de leitura de Json
+    public static String lerArquivoJson(String arquivoJson){
+        // TODO: Completar a leitura do arquivo 
+        // return new String(Files)
+        return null;
+    }
+
+    @Test
+    public void testPostPet(){
+        // carregar os dados do usuario do arquivo json do pet
+        // TODO: Criar o método Post
+        // String jsonBody = 
+    
+
+
+    }
+
+    
+
+
+
+}
