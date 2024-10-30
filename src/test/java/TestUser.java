6e57c933dd7fc7ca67c3944ffc2b2de8ed8e6a95
juyamada <jysyamada@gmail.com>
2024-10-29 22:41:08 -0300

    adionamos o test user

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,47 @@
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+
+public class TestUser{
+    String ct = "application/json";
+    String uriUser = "https://petstore.swagger.io/v2/user";
+
+@Test 
+    public void testLogin(){
+        //Configura
+        String username = "arya";
+        String password = "123456";
+
+        String resultadoEsperado = "logged in user session:";
+
+        Response resposta = (Response) given()  
+        .contentType(ct)
+        .log().all()
+
+        //Executa
+        .when()
+            .get(uriUser + "/login?username=" + username + "&password=" + password)
+
+        //Valida
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("code",is (200))
+            .body("type", is ("unknown"))
+            .body("message", containsString(resultadoEsperado))  //contem
+            .body("message", hasLength(36))  //
+        .extract()
+        ;
+        // extração
+        String token = resposta.jsonPath().getString("message").substring(16);
+        System.out.println("Conteúdo do Token:" + token);
+
+    }
+
+}
